d440d5155d9f26cbece6d01d9e661980ae504ec8
Ricardo <rirova81@gmail.com>
2021-11-30 20:38:40 +0100

    Add const let examples

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,13 @@
+// variables y constantes
+
+const NOMBRE = 'Ricardo';
+const APELLIDO = 'Rodriguez';
+
+let valorDado = 5;
+
+console.log(NOMBRE, APELLIDO, valorDado);
+
+if(true){
+    const NOMBRE = 'Juan';
+    console.log(NOMBRE);
+}
