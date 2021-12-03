2d3e352bfd9a692eb8def0317322d2eb288f3cea
Ricardo <rirova81@gmail.com>
2021-12-03 08:32:57 +0100

    Update 08-export-import.js

--- 02-intro-javascript/src/bases/08-export-import-data.js
+++ 02-intro-javascript/src/bases/08-export-import-data.js
@@ -1,10 +1,7 @@
-import { heroes } from "../data/heroes";
-
+import { heroes, owners } from '../data/heroes';
 
 const getHeroById = (id) => heroes.find((hero) => hero.id === id);
 console.log(getHeroById(2));
-console.log(getHeroById(3));
-console.log(getHeroById(1));
 
 const getHeroByOwner = (owner) => heroes.filter((hero) => hero.owner === owner);
-console.log(getHeroByOwner('DC'));
+console.log(getHeroByOwner(owners[0]));
--- 02-intro-javascript/src/index.js
+++
@@ -1,9 +0,0 @@
-import { heroes } from './data/heroes';
-
-const getHeroById = (id) => heroes.find((hero) => hero.id === id);
-console.log(getHeroById(2));
-console.log(getHeroById(3));
-console.log(getHeroById(1));
-
-const getHeroByOwner = (owner) => heroes.filter((hero) => hero.owner === owner);
-console.log(getHeroByOwner('DC'));
