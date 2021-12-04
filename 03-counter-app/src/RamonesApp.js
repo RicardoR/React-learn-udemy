307f9ac2245a2d6fd20681395645e75851b46d25
Ricardo <rirova81@gmail.com>
2021-12-04 13:17:47 +0100

    Update component

--- 03-counter-app/src/RamonesApp.js
+++ 03-counter-app/src/RamonesApp.js
@@ -1,7 +1,17 @@
-// Functional Component
+import React from 'react';
 
+// Functional Component
 const RamonesApp = () => {
-    return <h1>Hey Ho! Let's go!</h1>;
-}
+    return (
+      //<> </> ==> <Fragment></Fragment>
+    <>
+      <h1>Hey Ho! Let's go!</h1>
+      <p>They're forming in straight line</p>
+      <p>They're going through a tight wind</p>
+      <p>The kids are losing their minds</p>
+      <p>The blitzkrieg bop</p>
+    </>
+  );
+};
 
 export default RamonesApp;
