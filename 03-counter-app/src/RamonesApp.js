0b30480c401efe160c1e9f85e606246cec164080
Ricardo <rirova81@gmail.com>
2021-12-04 13:48:05 +0100

    Update RamonesApp.js

--- 03-counter-app/src/RamonesApp.js
+++ 03-counter-app/src/RamonesApp.js
@@ -2,16 +2,24 @@ import React from 'react';
 
 // Functional Component
 const RamonesApp = () => {
+    const ramones = {
+        band: 'Ramones',
+        origin: 'Los Angeles, California'
+    };
+    const numbers = [1, 2, 3, 4, 5];
+    
     return (
-      //<> </> ==> <Fragment></Fragment>
-    <>
-      <h1>Hey Ho! Let's go!</h1>
-      <p>They're forming in straight line</p>
-      <p>They're going through a tight wind</p>
-      <p>The kids are losing their minds</p>
-      <p>The blitzkrieg bop</p>
-    </>
-  );
+      // <> </> ==> <Fragment></Fragment>
+      <>
+        <h1>Hey Ho! Let's go!</h1>
+        <p>They're forming in straight line</p>
+        <p>They're going through a tight wind</p>
+        <p>The kids are losing their minds</p>
+        <p>The blitzkrieg bop</p>
+        <p> {ramones.band} - {ramones.origin} </p>
+        <p> {numbers} </p>
+      </>
+    );
 };
 
 export default RamonesApp;
