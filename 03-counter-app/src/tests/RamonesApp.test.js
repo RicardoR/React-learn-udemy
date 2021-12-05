91fb500e904d21b1748d7fa1636939084dc01d7c
Ricardo <rirova81@gmail.com>
2021-12-05 20:07:13 +0100

    Add enzyme and rewrite UT

--- 03-counter-app/src/tests/RamonesApp.test.js
+++ 03-counter-app/src/tests/RamonesApp.test.js
@@ -1,6 +1,9 @@
+// import { render } from '@testing-library/react';
+
 import React from 'react';
-import { render } from '@testing-library/react';
 import RamonesApp from '../RamonesApp';
+import { shallow } from 'enzyme';
+import '@testing-library/jest-dom';
 
 describe('<RamonesApp /> UT', () => {
   test('Should display Hey Ho! Lets go! in the title', () => {
@@ -9,10 +12,19 @@ describe('<RamonesApp /> UT', () => {
       band: 'Los Chichos!!',
       origin: 'Los Angeles, California',
     };
+
     const expectedTextByData = `${ramones.band} - ${ramones.origin} - Punk!`;
-      
-    const wrapper = render(<RamonesApp ramones={ramones} />);
-    expect(wrapper.getByText(title)).toBeInTheDocument();
-    expect(wrapper.getByText(expectedTextByData)).toBeInTheDocument();  
+
+    // Following code works whit import '@testing-library/jest-dom/extend-expect';
+    // const wrapper = render(<RamonesApp ramones={ramones} />);
+    // expect(wrapper.getByText(title)).toBeInTheDocument();
+    // expect(wrapper.getByText(expectedTextByData)).toBeInTheDocument();
+
+    const wrapper = shallow(<RamonesApp ramones={ramones} />);
+    expect(wrapper.find('h1').text()).toBe(title);
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('p').at(4).text()).toBe(expectedTextByData);
+    console.log(wrapper.find('p').last().text());
+    console.log(wrapper.find('p').at(4).text());
   });
 });
