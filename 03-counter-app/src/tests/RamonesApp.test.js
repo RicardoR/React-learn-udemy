3e517eb46d8fabd770cec044e27162f6d334431e
Ricardo <rirova81@gmail.com>
2021-12-05 20:17:38 +0100

    Update RamonesApp.test.js

--- 03-counter-app/src/tests/RamonesApp.test.js
+++ 03-counter-app/src/tests/RamonesApp.test.js
@@ -13,18 +13,31 @@ describe('<RamonesApp /> UT', () => {
       origin: 'Los Angeles, California',
     };
 
-    const expectedTextByData = `${ramones.band} - ${ramones.origin} - Punk!`;
-
     // Following code works whit import '@testing-library/jest-dom/extend-expect';
     // const wrapper = render(<RamonesApp ramones={ramones} />);
     // expect(wrapper.getByText(title)).toBeInTheDocument();
     // expect(wrapper.getByText(expectedTextByData)).toBeInTheDocument();
 
     const wrapper = shallow(<RamonesApp ramones={ramones} />);
-    expect(wrapper.find('h1').text()).toBe(title);
     expect(wrapper).toMatchSnapshot();
-    expect(wrapper.find('p').at(4).text()).toBe(expectedTextByData);
-    console.log(wrapper.find('p').last().text());
-    console.log(wrapper.find('p').at(4).text());
+    expect(wrapper.find('h1').text()).toBe(title);
+  });
+  
+  test('Should display the song and ramones data in the paragraphs', () => {
+    const ramones = {
+      band: 'Ramones!!',
+      origin: 'Los Angeles, California',
+    };
+
+    const expectedTextByData = `${ramones.band} - ${ramones.origin} - Punk!`;
+    const wrapper = shallow(<RamonesApp ramones={ramones} />);
+    
+    expect(wrapper.find('p').at(0).text()).toBe(
+      "They're forming in straight line"
+    );
+    expect(wrapper.find('p').at(1).text()).toBe(
+      "They're going through a tight wind"
+    );
+    expect(wrapper.find('p').last().text()).toBe(expectedTextByData);
   });
 });
