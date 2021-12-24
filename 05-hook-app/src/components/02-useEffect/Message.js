adc38aa1b81452a3969a02ae75ba5e07d52aed0b
Ricardo <rirova81@gmail.com>
2021-12-24 19:34:13 +0100

    Unmounting components

--- 05-hook-app/src/components/02-useEffect/Message.js
+++ 05-hook-app/src/components/02-useEffect/Message.js
@@ -1,17 +1,45 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Message = () => {
+    const [coords, setCoords] = useState({ x: 0, y: 0 });
 
+    const { x, y } = coords;
+    
     useEffect(() => {
-        console.log('componente mondato')
+
+        const mouseMove = (e) => { 
+            const newCoords = { x: e.x, y: e.y };
+            setCoords(newCoords);
+            console.log('Coords', newCoords);
+        }
+
+        window.addEventListener('mousemove', mouseMove);
+
         return () => {
             console.log('componente desmontado')
+            window.removeEventListener('mousemove', mouseMove);
         }
     }, [])
     
     return (
-        <div>
-            <h3>Soy el mensaje</h3>
+      <div>
+        <h3>Soy el mensaje</h3>
+        <div className="table-responsive">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>X</th>
+                <th>Y</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{x}</td>
+                <td>{y}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-    )
+      </div>
+    );
 }
