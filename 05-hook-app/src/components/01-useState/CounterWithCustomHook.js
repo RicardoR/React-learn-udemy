52e985d7b3b0518e4a55e73eeee317cff6384b24
Ricardo <rirova81@gmail.com>
2021-12-24 13:36:09 +0100

    Add counter custom hook

---
+++ 05-hook-app/src/components/01-useState/CounterWithCustomHook.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useCounter } from '../../hooks/useCounter';
+
+import './counter.css';
+
+export const CounterWithCustomHook = () => {
+
+    const {state: counter, increment, decrement, reset} = useCounter(50);
+
+    return (
+      <>
+        <h1>Counter with hook: {counter}</h1>
+        <hr />
+
+        <button className="btn" onClick={() => increment(2)}>
+          +1
+        </button>
+
+        <button className="btn" onClick={reset}>
+          Reset
+        </button>
+            
+        <button className="btn" onClick={() => decrement()}>
+          -1
+        </button>
+      </>
+    );
+}
