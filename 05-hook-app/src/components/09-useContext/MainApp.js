3703de014747a59e54d7d59ab9a4a4f5a06ec34f
Ricardo <rirova81@gmail.com>
2022-04-04 16:09:59 +0200

    Use context

--- 05-hook-app/src/components/09-useContext/MainApp.js
+++ 05-hook-app/src/components/09-useContext/MainApp.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Navbar } from './Navbar';
+import { UserContext } from './UserContext';
 
 export const MainApp = () => {
+  const user = {
+    id: 123,
+    name: 'Ricardo',
+    email: 'email@email.com',
+  };
   return (
     <div>
-      <Navbar />
-      {/* An <Outlet> renders whatever child route is currently active,
+      <UserContext.Provider value={user}>
+        <Navbar />
+        {/* An <Outlet> renders whatever child route is currently active,
           so you can think about this <Outlet> as a placeholder for
           the child routes we defined above. */}
-      <Outlet />
+        <Outlet />
+      </UserContext.Provider>
     </div>
   );
 };
