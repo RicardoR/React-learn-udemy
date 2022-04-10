561b4fcd63566b30bc005245c55db2132249ad86
Ricardo <rirova81@gmail.com>
2022-04-10 19:22:29 +0200

    Use context UT and serveral fixes

--- 05-hook-app/src/components/01-useState/CounterWithCustomHook.js
+++ 05-hook-app/src/components/01-useState/CounterWithCustomHook.js
@@ -1,28 +1,27 @@
-import React from 'react'
+import React from 'react';
 import { useCounter } from '../../hooks/useCounter';
 
 import './counter.css';
 
 export const CounterWithCustomHook = () => {
+  const { counter, increment, decrement, reset } = useCounter(50);
 
-    const {counter, increment, decrement, reset} = useCounter(50);
+  return (
+    <>
+      <h1>Counter with hook: {counter}</h1>
+      <hr />
 
-    return (
-      <>
-        <h1>Counter with hook: {counter}</h1>
-        <hr />
+      <button className="btn" onClick={() => increment(2)}>
+        +2
+      </button>
 
-        <button className="btn" onClick={() => increment(2)}>
-          +2
-        </button>
+      <button className="btn" onClick={reset}>
+        Reset
+      </button>
 
-        <button className="btn" onClick={reset}>
-          Reset
-        </button>
-            
-        <button className="btn" onClick={() => decrement()}>
-          -1
-        </button>
-      </>
-    );
-}
+      <button className="btn" onClick={() => decrement()}>
+        -1
+      </button>
+    </>
+  );
+};
