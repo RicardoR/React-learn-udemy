c92c7b183ddcbd505450997c2a8e40fac7eba4d1
Ricardo <rirova81@gmail.com>
2022-04-15 10:35:54 +0200

    TodoListItem UT

---
+++ 05-hook-app/src/tests/components/08-useReducer/TodoListItem.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { TodoListItem } from '../../../components/08-useReducer/TodoListItem';
+import { demoTodos } from '../../fixtures/demoTodos';
+
+describe('TodoListItem UT', () => {
+  const handleComplete = jest.fn();
+  const handleRemove = jest.fn();
+
+  const wrapper = shallow(
+    <TodoListItem
+      todo={demoTodos[0]}
+      index={0}
+      handleComplete={handleComplete}
+      handleRemove={handleRemove}
+    />
+  );
+
+  test('should render correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test('should call handleComplete when clicked on todo', () => {
+    wrapper.find('p').simulate('click');
+    expect(handleComplete).toHaveBeenCalledWith(demoTodos[0].id);
+  });
+
+  test('should call handleRemove when clicked on button', () => {
+    wrapper.find('button').simulate('click');
+    expect(handleRemove).toHaveBeenCalledWith(demoTodos[0].id);
+  });
+
+  test('should display the todo description', () => {
+    expect(wrapper.find('p').text().trim()).toBe(`1. ${demoTodos[0].desc}`);
+  });
+});
