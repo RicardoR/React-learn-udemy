a0e5a13c7e13323bc809263a89547bbb2a292339
Ricardo <rirova81@gmail.com>
2022-04-17 21:20:28 +0200

    Navigate between routes

--- 06-heroes-app/src/components/login/LoginScreen.js
+++ 06-heroes-app/src/components/login/LoginScreen.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const LoginScreen = () => {
-  return <div>LoginScreen</div>;
+  const navigate = useNavigate();
+  const handleLogin = () => {
+    // replace true evita que puedas volver atrás a esta pantalla
+    navigate('/', { replace: true });
+  };
+
+  return (
+    <div className="container mt-5">
+      <h1>LoginScreen</h1>
+      <hr />
+      <button className="btn btn-primary" onClick={handleLogin}>
+        Login
+      </button>
+    </div>
+  );
 };
--- 06-heroes-app/src/components/ui/Navbar.js
+++ 06-heroes-app/src/components/ui/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 export const Navbar = () => {
+  const navigate = useNavigate();
   const handleLogout = () => {
-    // todo: logout
-    console.log('logout');
+    navigate('/login', { replace: true });
   };
 
   return (
