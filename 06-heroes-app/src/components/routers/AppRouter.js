1886de6b4aba86f143db6612312313de0228e50f
Ricardo <rirova81@gmail.com>
2022-04-17 21:09:07 +0200

    Add multiple router

--- 06-heroes-app/src/components/routers/AppRouter.js
+++ 06-heroes-app/src/components/routers/AppRouter.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import { DcScreen } from '../dc/DcScreen';
 import { LoginScreen } from '../login/LoginScreen';
-import { MarvelScreen } from '../marvel/MarvelScreen';
-import { SearchScreen } from '../search/SearchScreen';
-import { Navbar } from '../ui/Navbar';
+import { DashboardRoutes } from './DashboardRoutes';
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<MarvelScreen />} />
-        <Route path="/marvel" element={<MarvelScreen />} />
-        <Route path="/dc" element={<DcScreen />} />
         <Route path="/login" element={<LoginScreen />} />
-        <Route path="/search" element={<SearchScreen />} />
+        <Route path="/*" element={<DashboardRoutes />} />
       </Routes>
     </BrowserRouter>
   );
