3ac897855d6f324f132f71a353c2f0bf4e7e986b
Ricardo <rirova81@gmail.com>
2022-05-02 19:42:06 +0200

    Use react 17 and enzyme

--- 06-heroes-app/src/index.js
+++ 06-heroes-app/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { HeroesApp } from './HeroesApp';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<HeroesApp />);
+ReactDOM.render(<HeroesApp />, document.getElementById('root'));
