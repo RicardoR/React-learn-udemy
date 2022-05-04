328b9efce33458446d35ddfc947b7266eb629dcd
Ricardo <rirova81@gmail.com>
2022-05-04 08:29:36 +0200

    Add ut for logged user in AppRouter component

--- 06-heroes-app/src/test/routers/AppRouter.test.js
+++ 06-heroes-app/src/test/routers/AppRouter.test.js
@@ -3,13 +3,13 @@ import { AuthContext } from '../../auth/authContext';
 import { AppRouter } from '../../components/routers/AppRouter';
 
 describe('AppRouter UT', () => {
-  const contextValue = {
-    user: {
-      logged: false,
-    },
-  };
-
   test('when no auth should display login screen', () => {
+    const contextValue = {
+      user: {
+        logged: false,
+      },
+    };
+
     const wrapper = mount(
       <AuthContext.Provider value={contextValue}>
         <AppRouter />
@@ -18,5 +18,23 @@ describe('AppRouter UT', () => {
     expect(wrapper).toMatchSnapshot();
     expect(wrapper.find('LoginScreen')).toHaveLength(1);
     expect(wrapper.find('h1').text().trim()).toBe('Login');
+  });
+
+  test('should display Marvel Component when user is logged', () => {
+    const contextValue = {
+      user: {
+        logged: true,
+        name: 'Ricardo',
+      },
+    };
+    const wrapper = mount(
+      <AuthContext.Provider value={contextValue}>
+        <AppRouter />
+      </AuthContext.Provider>
+    );
+
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('.navbar').exists()).toBeTruthy();
+    expect(wrapper.find('MarvelScreen').exists()).toBeTruthy();
   });
 });
