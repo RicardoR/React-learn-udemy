aa0db897807abc7791bcad7a267be111e7213ff1
Ricardo <rirova81@gmail.com>
2022-05-04 22:09:21 +0200

    Update SearchScreen UT

--- 06-heroes-app/src/test/components/SearchScreen.test.js
+++ 06-heroes-app/src/test/components/SearchScreen.test.js
@@ -1,5 +1,6 @@
 import { mount } from 'enzyme';
 import { MemoryRouter } from 'react-router-dom';
+import { HeroCard } from '../../components/hero/HeroCard';
 import { SearchScreen } from '../../components/search/SearchScreen';
 
 describe('SearchScreen UT', () => {
@@ -11,5 +12,29 @@ describe('SearchScreen UT', () => {
     );
     expect(wrapper).toMatchSnapshot();
     expect(wrapper.find('.alert-info').text().trim()).toBe('Buscar un héroe');
+  });
+
+  test('should display Batman and input with the query string', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/search?q=batman']}>
+        <SearchScreen />
+      </MemoryRouter>
+    );
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('input').prop('value')).toBe('batman');
+    expect(wrapper.find(HeroCard)).toHaveLength(1);
+  });
+
+  test('should display a message when search does not found any result', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/search?q=no-hero']}>
+        <SearchScreen />
+      </MemoryRouter>
+    );
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('input').prop('value')).toBe('no-hero');
+    expect(wrapper.find('.alert-danger').text().trim()).toBe(
+      'No hay resultados para: no-hero'
+    );
   });
 });
