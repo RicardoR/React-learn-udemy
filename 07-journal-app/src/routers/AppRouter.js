5247df21ab929210e4c8023fb63957b24aa811d0
Ricardo <rirova81@gmail.com>
2022-05-15 18:29:57 +0200

    Create basic structure for styles

--- 07-journal-app/src/routers/AppRouter.js
+++ 07-journal-app/src/routers/AppRouter.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import { JournalScreen } from '../components/journal/JournalScreen';
 import { AuthRouter } from './AuthRouter';
 
