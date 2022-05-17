b085a4e1cdec3d950005154bfbc4455f5c457165
Ricardo <rirova81@gmail.com>
2022-05-17 22:19:55 +0200

    Add notes screen

--- 07-journal-app/src/components/journal/JournalScreen.js
+++ 07-journal-app/src/components/journal/JournalScreen.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Sidebar } from './Sidebar';
 import { EmptyState } from './EmptyState';
+import { NoteScreen } from '../notes/NoteScreen';
 
 export const JournalScreen = () => {
   return (
     <div className="journal__main-content">
       <Sidebar />
       <main>
-        <EmptyState />
+        <NoteScreen />
+        {/* <EmptyState /> */}
       </main>
     </div>
   );
