3f1e472e671b9e51237c88904b1a53b178730806
Ricardo <rirova81@gmail.com>
2022-05-22 12:02:59 +0200

    Update RegisterScreen.js

--- 07-journal-app/src/components/auth/RegisterScreen.js
+++ 07-journal-app/src/components/auth/RegisterScreen.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
 
 export const RegisterScreen = () => {
+  const [formValues, handleInputChanges] = useForm({
+    name: 'Ricardo',
+    email: 'email@gmail.com',
+    password: '123456',
+    confirmPassword: '123456',
+  });
+
+  const { name, email, password, confirmPassword } = formValues;
+
+  const handeRegister = (e) => {
+    e.preventDefault();
+  };
+
+  const isFormValid = () => {
+    if (password !== confirmPassword) {
+      return false;
+    }
+  };
+
   return (
     <>
       <h3 className="auth__title mb-5">Register</h3>
-      <form>
+      <form onSubmit={handeRegister}>
         <input
           type="text"
           placeholder="Name"
           name="name"
           className="auth__input"
           autoComplete="off"
+          value={name}
+          onChange={handleInputChanges}
         />
         <input
           type="email"
@@ -19,6 +41,8 @@ export const RegisterScreen = () => {
           name="email"
           className="auth__input"
           autoComplete="off"
+          value={email}
+          onChange={handleInputChanges}
         />
         <input
           type="password"
@@ -26,6 +50,8 @@ export const RegisterScreen = () => {
           name="password"
           className="auth__input"
           autoComplete="off"
+          value={password}
+          onChange={handleInputChanges}
         />
         <input
           type="password"
@@ -33,6 +59,8 @@ export const RegisterScreen = () => {
           name="confirmPassword"
           className="auth__input"
           autoComplete="off"
+          value={confirmPassword}
+          onChange={handleInputChanges}
         />
         <button type="submit" className="btn btn-primary btn-block mb-5 mt-1">
           Login
