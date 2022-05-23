dce6e1ac1d725fd2de0c91926b0b439200a646ce
Ricardo <rirova81@gmail.com>
2022-05-23 08:23:36 +0200

    Listen to authStateChanged in firebase

--- 07-journal-app/src/routers/AppRouter.js
+++ 07-journal-app/src/routers/AppRouter.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from 'react-router-dom';
+import { login } from '../actions/auth';
 import { JournalScreen } from '../components/journal/JournalScreen';
 import { AuthRouter } from './AuthRouter';
 
 export const AppRouter = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const auth = getAuth();
+
+    onAuthStateChanged(auth, (user) => {
+      if (user?.uid) {
+        dispatch(login(user.uid, user.displayName));
+      }
+    });
+  }, [dispatch]);
+
   return (
     <Router>
       <div>
