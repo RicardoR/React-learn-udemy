e5553e6133e99f6e6dec58d3a1a851afa8083a22
Ricardo <rirova81@gmail.com>
2022-05-24 08:28:41 +0200

    Add notes reducer

--- 07-journal-app/src/components/journal/JournalScreen.js
+++ 07-journal-app/src/components/journal/JournalScreen.js
@@ -1,16 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Sidebar } from './Sidebar';
 import { EmptyState } from './EmptyState';
 import { NoteScreen } from '../notes/NoteScreen';
 
 export const JournalScreen = () => {
+  const { active } = useSelector((state) => state.notes);
+
   return (
     <div className="journal__main-content">
       <Sidebar />
-      <main>
-        <NoteScreen />
-        {/* <EmptyState /> */}
-      </main>
+      <main>{active ? <NoteScreen /> : <EmptyState />}</main>
     </div>
   );
 };
---
+++ 07-journal-app/src/reducers/notesReducer.js
@@ -0,0 +1,24 @@
+/* 
+{
+    notes: [],
+    active?:  {
+        id: '',
+        title: '',
+        body: '',
+        imageUrl: '',
+        date: '',
+    }
+ }
+ */
+
+const initialState = {
+  notes: [],
+  active: null,
+};
+
+export const notesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
