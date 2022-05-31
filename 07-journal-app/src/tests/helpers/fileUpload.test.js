c11d2002117beb04d438fda8dc5ba5f35e6721ed
Ricardo <rirova81@gmail.com>
2022-05-31 19:52:30 +0200

    delete images after in UT

--- 07-journal-app/src/tests/helpers/fileUpload.test.js
+++ 07-journal-app/src/tests/helpers/fileUpload.test.js
@@ -1,4 +1,15 @@
+import 'setimmediate';
+import cloudinary from 'cloudinary';
+
 import { fileUpload } from '../../helpers/fileUpload';
+import { cloudName, apiKey, apiSecret } from '../../secret/cloudbinary';
+
+cloudinary.config({
+  cloud_name: `${cloudName}`,
+  api_key: `${apiKey}`,
+  api_secret: `${apiSecret}`,
+  secure: true,
+});
 
 describe('fileUpload UT', () => {
   test('should load a file and return a secure url', async () => {
@@ -10,6 +21,11 @@ describe('fileUpload UT', () => {
     const url = await fileUpload(file);
 
     expect(typeof url).toBe('string');
+
+    const segments = url.split('/');
+    const imageID = segments[segments.length - 1].replace('.png', '');
+
+    cloudinary.v2.api.delete_resources(`${imageID}`, {}, () => {});
   });
 
   test('should return an error', async () => {
