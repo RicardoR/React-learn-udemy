1b6c102da9c335e0aa16929c9074c5c572f87257
Ricardo <rirova81@gmail.com>
2022-06-12 12:30:04 +0200

    Init calendar App

---
+++ 08-calendar-app/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { CalendarApp } from './components/CalendarApp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<CalendarApp />);
