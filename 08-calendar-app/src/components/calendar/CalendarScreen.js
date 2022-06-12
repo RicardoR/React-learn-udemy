8a9dcb009a080b09a30ccc69de59b5e04cfd9423
Ricardo <rirova81@gmail.com>
2022-06-12 16:23:19 +0200

    Personalisation

--- 08-calendar-app/src/components/calendar/CalendarScreen.js
+++ 08-calendar-app/src/components/calendar/CalendarScreen.js
@@ -3,9 +3,13 @@ import React from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import { Navbar } from '../ui/Navbar';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { messages } from '../../helpers/calendar-messages-es';
 
-const localizer = momentLocalizer(moment); // or globalizeLocalizer
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'moment/locale/es';
+
+moment.locale('es');
+const localizer = momentLocalizer(moment);
 
 const events = [
   {
@@ -18,14 +22,27 @@ const events = [
 ];
 
 export const CalendarScreen = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: event.bgColor,
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white',
+      border: '0px',
+    };
+    return { style };
+  };
+
   return (
-    <div className='calendar-screen'>
+    <div className="calendar-screen">
       <Navbar />
       <Calendar
         localizer={localizer}
         events={events}
         startAccessor="start"
         endAccessor="end"
+        messages={messages}
+        eventPropGetter={eventStyleGetter}
       />
     </div>
   );
