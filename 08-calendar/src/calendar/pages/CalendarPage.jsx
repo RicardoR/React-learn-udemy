277f73a70256d10fcdbf8fd501731bb141584459
Ricardo <rirova81@gmail.com>
2022-06-16 08:18:26 +0200

    Add react modal

--- 08-calendar/src/calendar/pages/CalendarPage.jsx
+++ 08-calendar/src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,10 @@
+import { useState } from 'react';
 import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { addHours } from 'date-fns';
 
-import { Navbar, CalendarEvent } from '../';
+import { Navbar, CalendarEvent, CalendarModal } from '../';
 import { getMessagesES, localizer } from '../../helpers';
-import { useState } from 'react';
 
 const events = [
   {
@@ -70,6 +70,8 @@ export const CalendarPage = () => {
         onSelectEvent={onSelect}
         onView={onViewChanged}
       />
+
+      <CalendarModal />
     </>
   );
 };
