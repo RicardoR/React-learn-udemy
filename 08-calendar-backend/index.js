877a393132f0deba7f23271321aa83ac3dd3b952
Ricardo <rirova81@gmail.com>
2022-06-27 20:29:10 +0200

    First express configuration

--- 08-calendar-backend/index.js
+++ 08-calendar-backend/index.js
@@ -1,1 +1,16 @@
-console.log('hola mundo!!!');
+const express = require('express');
+
+// Create express server
+const app = express();
+
+// Routes
+app.get('/', (request, response) => {
+  response.json({
+    ok: true,
+  });
+});
+
+// Listen to request
+app.listen(4000, () => {
+  console.log(`Server running on port ${4000}`);
+});
