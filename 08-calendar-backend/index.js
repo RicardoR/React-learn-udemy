84d29df18bda1f3c2edf1ef241a46acbf5249937
Ricardo <rirova81@gmail.com>
2022-06-27 20:48:25 +0200

    set public configuration and .env variable

--- 08-calendar-backend/index.js
+++ 08-calendar-backend/index.js
@@ -1,16 +1,20 @@
 const express = require('express');
+require('dotenv').config();
 
 // Create express server
 const app = express();
 
+// Public directory
+app.use(express.static('public'));
+
 // Routes
-app.get('/', (request, response) => {
-  response.json({
-    ok: true,
-  });
-});
+// app.get('/', (request, response) => {
+//   response.json({
+//     ok: true,
+//   });
+// });
 
 // Listen to request
-app.listen(4000, () => {
-  console.log(`Server running on port ${4000}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
