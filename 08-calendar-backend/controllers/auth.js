79c3c1e78ecc561acafab4b24b45ac7ce754b177
Ricardo <rirova81@gmail.com>
2022-07-03 10:07:46 +0200

    Add user model and create user when register

--- 08-calendar-backend/controllers/auth.js
+++ 08-calendar-backend/controllers/auth.js
@@ -1,15 +1,25 @@
 const { response } = require('express');
+const User = require('../models/User');
 
-const createUser = (request, resp = response) => {
-  const { name, email, password } = request.body;
+const createUser = async (request, resp = response) => {
+  //const { name, email, password } = request.body;
 
-  resp.status(201).json({
-    ok: true,
-    msg: 'registro',
-    name,
-    email,
-    password,
-  });
+  try {
+    const newUser = new User(request.body);
+
+    await newUser.save();
+
+    resp.status(201).json({
+      ok: true,
+      msg: 'registro',
+    });
+  } catch (err) {
+    console.log(err);
+    resp.status(500).json({
+      ok: false,
+      msg: `Error creating user: ${err.code}`,
+    });
+  }
 };
 
 const performLogin = (request, resp = response) => {
