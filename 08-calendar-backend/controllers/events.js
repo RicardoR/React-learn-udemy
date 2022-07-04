16f3717bbc9dd82e31b07cb23908ac3db4699551
Ricardo <rirova81@gmail.com>
2022-07-04 17:25:14 +0200

    Store in db

--- 08-calendar-backend/controllers/events.js
+++ 08-calendar-backend/controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require('express');
+const Event = require('../models/Event');
 
 const getEvents = (req, resp = response) => {
   return resp.status(201).json({
@@ -7,13 +8,23 @@ const getEvents = (req, resp = response) => {
   });
 };
 
-const createEvent = (req, resp = response) => {
-  console.log(req.body);
+const createEvent = async (req, resp = response) => {
+  const event = new Event(req.body);
 
-  return resp.status(201).json({
-    ok: true,
-    msg: 'createEvents',
-  });
+  try {
+    event.user = req.uid;
+    const savedEvent = await event.save();
+    return resp.status(201).json({
+      ok: true,
+      evento: savedEvent,
+    });
+  } catch (error) {
+    console.log(error);
+    return resp.status(500).json({
+      ok: false,
+      msg: 'Could not create event',
+    });
+  }
 };
 
 const updateEvent = (req, resp = response) => {
