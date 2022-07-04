8fc7afe66e1269b084a185b2e8290b750aeddbcc
Ricardo <rirova81@gmail.com>
2022-07-04 16:45:58 +0200

    Validate dates

--- 08-calendar-backend/routes/events.js
+++ 08-calendar-backend/routes/events.js
@@ -11,6 +11,9 @@ const {
 } = require('../controllers/events');
 const { validateJWT } = require('../middlewares/validate-jwt');
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { fieldValidator } = require('../middlewares/field-validator');
+const { isDate } = require('../helpers/isDate');
 const router = Router();
 
 // All routes need to validate token
@@ -18,7 +21,16 @@ router.use(validateJWT);
 
 router.get('/', getEvents);
 
-router.post('/', createEvent);
+router.post(
+  '/',
+  [
+    check('title', 'Title is required').not().isEmpty(),
+    check('start', 'Start date is mandatoy').custom(isDate),
+    check('end', 'End date is mandatoy').custom(isDate),
+    fieldValidator,
+  ],
+  createEvent
+);
 
 router.put('/:id', updateEvent);
 
