d869f94171f3624a190daee1b2b149907168ef75
Ricardo <rrodriguez@areaseys.com>
2022-07-19 19:22:30 +0200

    Set vars to component and styles

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelloWorldApp } from './HelloWorldApp';
 import {FirstApp} from "./FirstApp";
+import "./styles.css";
 
+const developer = {
+    name: 'Ricardo',
+    surname: 'Rodríguez',
+    age: 41
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <HelloWorldApp/>
-        <FirstApp/>
+        <FirstApp {...developer} />
     </React.StrictMode>
 );
