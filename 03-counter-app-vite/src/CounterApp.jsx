e4dc5a5ee5ded495ab89d3940a7b81a5eb877953
Ricardo <rrodriguez@areaseys.com>
2022-07-20 18:57:22 +0200

    Add useState in CounterApp

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+ import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
 export const CounterApp = ({value}) => {
+
+    const [counter, updateCounter] = useState(value);
+
+    const updateValue = (val) => {
+        updateCounter(() => counter + val )
+    };
+
     return (
         <>
             <h1> CounterApp</h1>
-            <h2>{value}</h2>
+            <h2> { counter } </h2>
+            <button onClick={() => updateValue(1)}>+1</button>
+            <button onClick={() => updateValue(2)}>+2</button>
+            <button onClick={() => updateValue(-1)}>-1</button>
+            <button onClick={() => updateValue(-2)}>-2</button>
         </>
     );
 };
