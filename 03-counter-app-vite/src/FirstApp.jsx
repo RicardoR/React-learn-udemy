544122c78d61979745f210e8eec2de1ec5657120
Ricardo <rrodriguez@areaseys.com>
2022-07-20 18:22:16 +0200

    PropTypes

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const getSum = (a,b) => {
     return a + b;
 };
 
 export const FirstApp = ({name, surname, age}) => {
-
-
     return (
         <>
             <h1> Name: { name } </h1>
@@ -16,4 +15,11 @@ export const FirstApp = ({name, surname, age}) => {
             <div> Sum: { getSum(2,2) } </div>
         </>
     );
+};
+
+
+FirstApp.propTypes = {
+    name: PropTypes.string.isRequired,
+    surname: PropTypes.string.isRequired,
+    age: PropTypes.number.isRequired
 };
