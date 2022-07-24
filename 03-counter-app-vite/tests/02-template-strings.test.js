44381310f46a231476c4e0376504d77b1af69381
Ricardo <rrodriguez@areaseys.com>
2022-07-24 13:07:07 +0200

    03-counter-app-vite template strings UT

---
+++ 03-counter-app-vite/tests/02-template-strings.test.js
@@ -0,0 +1,10 @@
+import { getSaludo } from "../src/base-pruebas/02-template-string";
+
+describe('02-template-strings UT', () => {
+    test('getSaludo should return hola + nombre', () => {
+       const name = 'Ricardo';
+       const expectedGreeting = 'Hola Ricardo';
+       const greeting = getSaludo(name);
+       expect(greeting).toBe(expectedGreeting);
+    });
+});
