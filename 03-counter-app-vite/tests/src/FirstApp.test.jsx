4ca71a007e29116639c773092ac367286538f73f
Ricardo <rrodriguez@areaseys.com>
2022-07-24 19:01:04 +0200

    03-counter-app-vite vite UT FirstApp Ut

--- 03-counter-app-vite/tests/src/FirstApp.test.jsx
+++ 03-counter-app-vite/tests/src/FirstApp.test.jsx
@@ -8,6 +8,19 @@ describe('FirstApp UT', () => {
             surname: 'Rodríguez',
             age: 41
         };
-        render(<FirstApp {...developer} />)
+        const {container} = render(<FirstApp {...developer} />);
+        expect(container).toMatchSnapshot();
+    });
+
+    test('should display the name in h1 tag', async () => {
+        const developer = {
+            name: 'Ricardo',
+            surname: 'Rodríguez',
+            age: 41
+        };
+        const {container, getByText} = await render(<FirstApp {...developer} />);
+        expect(getByText('Name: Ricardo', { exact: false })).toBeTruthy();
+        const h1Element = container.querySelector('h1');
+        expect(h1Element.innerHTML).toContain('Name: Ricardo');
     });
 });
