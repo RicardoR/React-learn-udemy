5280a57a65e6451de6dc15fd2a3dba783a02272d
Ricardo <rrodriguez@areaseys.com>
2022-07-26 19:20:37 +0200

    04-gif-expert-app-vite setCategories

--- 04-gif-expert-app-vite/src/GifExpertApp.jsx
+++ 04-gif-expert-app-vite/src/GifExpertApp.jsx
@@ -3,9 +3,14 @@ import { useState } from "react";
 export const GifExpertApp = () => {
     const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
 
+    const onAddCategory = () => {
+        setCategories(categoryList => ['New Category', ...categoryList])
+    };
+
     return (
         <>
             <h1> GifExpertApp</h1>
+            <button onClick={onAddCategory}>Add category</button>
             <ol>
                 {
                     categories.map((category, index) => <li key={index}>{category}</li>)
