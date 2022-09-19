19cba229f6d1540bfaad80a16b44f7d44d16d3d1
Ricardo <rrodriguez@areaseys.com>
2022-09-19 08:24:46 +0200

    04-gif-expert-app-vite add GifGridItem Ut

--- 04-gif-expert-app-vite/test/components/GifGridItem.test.jsx
+++ 04-gif-expert-app-vite/test/components/GifGridItem.test.jsx
@@ -1,10 +1,26 @@
-import {render} from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
 import {GifGridItem} from "../../src/components";
 
 describe('GifGridItem UT', () => {
-    const {container} = render(<GifGridItem url='url' title='title'/>);
 
     test('should do match with snapshot', () => {
+        const {container} = render(<GifGridItem url='image-url' title='title-image'/>);
         expect(container).toMatchSnapshot();
    });
+
+    test('should display the image with url and alt', () => {
+        render(<GifGridItem url='image-url' title='title-image'/>);
+
+        const { src, alt } = screen.getByRole('img');
+
+        expect(src).toBe('http://localhost/image-url');
+        expect(alt).toBe('title-image');
+
+        // expect(screen.getByRole('img').alt).toBe('title-image');
+    });
+
+    test('should display the component title', () => {
+        render(<GifGridItem url='image-url' title='title-image'/>);
+        expect(screen.getByText('title-image')).toBeTruthy();
+    });
 });
