3f57fe510c74c47b80b4dee9df8fe22c1add5507
Ricardo <ricardo.rv@outlook.com>
2022-09-26 08:27:39 +0200

    04-gif-expert-app-vite AddCategory form Ut

--- 04-gif-expert-app-vite/test/components/AddCategory.test.jsx
+++ 04-gif-expert-app-vite/test/components/AddCategory.test.jsx
@@ -1,4 +1,4 @@
-import {render, screen, fireEvent} from "@testing-library/react";
+import {fireEvent, render, screen} from "@testing-library/react";
 import {AddCategory} from "../../src/components";
 
 describe('AddCategory UT', () => {
@@ -10,7 +10,18 @@ describe('AddCategory UT', () => {
     test('should manage the input text value', () => {
         render(<AddCategory onNewCategory={ () => {} }/>);
         const input = screen.getByRole('textbox');
-        fireEvent.input(input, { target : {value: 'bleach' } });
+        fireEvent.input(input, { target : { value: 'bleach' } });
         expect(input.value).toBe('bleach');
+    });
+
+    test('should call onNewCategory when input contains value', () => {
+        const inputValue = 'Dragon Ball';
+        render(<AddCategory onNewCategory={() => {}}/>);
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+        fireEvent.input(input, { target : { value: inputValue } });
+        fireEvent.submit(form);
+
+        expect(input.value).toBe('');
     });
 });
