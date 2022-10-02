71355a162673d67fa26a44a49b33cdf9ab0cfa74
Ricardo <rirova81@gmail.com>
2022-10-02 20:00:48 +0200

    05-hook-app-vite add simpleForm

---
+++ 05-hook-app-vite/src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,55 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+export const SimpleForm = () => {
+  const [formState, setFormState] = useState({
+    username: 'ricardo',
+    email: 'email@mail.com',
+  });
+
+  const { username, email } = formState;
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  useEffect(() => {
+    console.log('Email was changed!');
+  }, [email]);
+
+  useEffect(() => {
+    console.log('Username was changed!');
+  }, [username]);
+
+  useEffect(() => {
+    console.log('Component was mounted!');
+  }, []);
+
+  return (
+    <>
+      <h1>Simple form</h1>
+      <hr />
+
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Username"
+        name="username"
+        value={username}
+        onChange={onInputChange}
+      />
+
+      <input
+        type="email"
+        className="form-control mt-2"
+        placeholder="Email"
+        name="email"
+        value={email}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
--- 05-hook-app-vite/src/main.jsx
+++ 05-hook-app-vite/src/main.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import { CounterwithCustomHook } from './01-useState/CounterwithCustomHook';
+// import { CounterwithCustomHook } from './01-useState/CounterwithCustomHook';
 // import { CounterApp } from './01-useState/CounterApp';
 // import { HooksApp } from './HooksApp.jsx';
+import { SimpleForm } from './02-useEffect/SimpleForm';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CounterwithCustomHook />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <SimpleForm />
+  // </React.StrictMode>
 );
