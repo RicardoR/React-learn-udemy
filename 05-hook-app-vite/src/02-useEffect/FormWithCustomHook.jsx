a0d44c0cc2a8c5ea31ac9b78ee3681e7790785b8
Ricardo <ricardo.rv@outlook.com>
2022-10-26 08:16:47 +0200

    05-hook-app-vite create custom hook named useForm

---
+++ 05-hook-app-vite/src/02-useEffect/FormWithCustomHook.jsx
@@ -0,0 +1,53 @@
+import {useForm} from "../hooks/useForm.js";
+
+export const FormWithCustomHook = () => {
+
+    const {onInputChange, username, email, password} = useForm({
+        username: '',
+        email: '',
+        password: ''
+    });
+    
+    // const {formState, onInputChange} = useForm({
+    //     username: '',
+    //     email: '',
+    //     password: ''
+    // });
+    //
+    // const {username, email, password} = formState;
+
+    return (
+        <>
+            <h1>Form with custom hook</h1>
+            <hr/>
+
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={onInputChange}
+            />
+
+            <input
+                type="email"
+                className="form-control mt-2"
+                placeholder="Email"
+                name="email"
+                value={email}
+                onChange={onInputChange}
+            />
+
+            <input
+                type="password"
+                className="form-control mt-2"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={onInputChange}
+            />
+
+        </>
+    );
+};
--- 05-hook-app-vite/src/main.jsx
+++ 05-hook-app-vite/src/main.jsx
@@ -5,10 +5,11 @@ import './index.css';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 // import { CounterApp } from './01-useState/CounterApp';
 // import { HooksApp } from './HooksApp.jsx';
-import { SimpleForm } from './02-useEffect/SimpleForm';
+// import { SimpleForm } from './02-useEffect/SimpleForm';
+import {FormWithCustomHook} from './02-useEffect/FormWithCustomHook';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <SimpleForm />
-  // </React.StrictMode>
+    // <React.StrictMode>
+    <FormWithCustomHook/>
+    // </React.StrictMode>
 );
