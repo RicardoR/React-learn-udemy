c84db3bfa51c80402b0d1b9dc4211abfab3b3b01
Ricardo <ricardo.rv@outlook.com>
2022-10-28 08:39:22 +0200

    05-hook-app-vite read data from quote in MultipleCustomHooks

--- 05-hook-app-vite/src/03-example/MultipleCustomHooks.jsx
+++ 05-hook-app-vite/src/03-example/MultipleCustomHooks.jsx
@@ -4,11 +4,31 @@ import {useFetch} from "../hooks/useFetch.js";
 export const MultipleCustomHooks = () => {
 
     const {data, isLoading, hasError} = useFetch('https://www.breakingbadapi.com/api/quotes/1');
+    const {author, quote} = !!data && data[0];
 
-    console.log(data, isLoading, hasError)
     return (
         <>
             <h1> breaking bad Quotes</h1>
+            <hr/>
+
+            {
+                isLoading
+                    ? (
+                        <div className="alert alert-info text-center">
+                            Loading...
+                        </div>
+                    )
+                    : (
+                        <blockquote className="blockquote text-end">
+                            <p className="mb-1"> {quote} </p>
+                            <footer className="blockquote-footer">{author}</footer>
+                        </blockquote>
+                    )
+            }
+
+            <button className='btn btn-primary'>
+                Next quote
+            </button>
         </>
     );
 };
