bfa846b2e5ba225ed18a53f5c29ba0558fc6aba3
Ricardo <ricardo.rv@outlook.com>
2022-10-29 09:54:39 +0200

    05-hook-app-vite create FocusScreen.jsx

--- 05-hook-app-vite/src/main.jsx
+++ 05-hook-app-vite/src/main.jsx
@@ -7,10 +7,11 @@ import './index.css';
 // import { HooksApp } from './HooksApp.jsx';
 // import { SimpleForm } from './02-useEffect/SimpleForm';
 // import {FormWithCustomHook} from './02-useEffect/FormWithCustomHook';
-import {MultipleCustomHooks} from "./03-example/MultipleCustomHooks.jsx";
+// import {MultipleCustomHooks} from "./03-example/MultipleCustomHooks.jsx";
+import {FocusScreen} from "./04-useRef/FocusScreen.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     // <React.StrictMode>
-    <MultipleCustomHooks/>
+    <FocusScreen/>
     // </React.StrictMode>
 );
