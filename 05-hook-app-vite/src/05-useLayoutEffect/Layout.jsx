81eb0fc22f252fbb6aeff18d33a5508d18742069
Ricardo <ricardo.rv@outlook.com>
2022-10-29 10:08:44 +0200

    05-hook-app-vite useLayoutEffect example in Quote.jsx

---
+++ 05-hook-app-vite/src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,32 @@
+import {useCounter, useFetch} from "../hooks";
+import {LoadingQuote, Quote} from "../03-example/";
+
+
+export const Layout = () => {
+
+    const {counter, increment} = useCounter(1)
+    const {data, isLoading, hasError} = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
+    const {author, quote} = !!data && data[0];
+
+    return (
+        <>
+            <h1> breaking bad Quotes</h1>
+            <hr/>
+
+            {
+                isLoading
+                    ? <LoadingQuote></LoadingQuote>
+                    : <Quote quote={quote} author={author}></Quote>
+            }
+
+            <button className='btn btn-primary'
+                    disabled={isLoading}
+                    onClick={() => increment()}>
+                Next quote
+            </button>
+        </>
+    );
+};
+
+
+
--- 05-hook-app-vite/src/main.jsx
+++ 05-hook-app-vite/src/main.jsx
@@ -8,10 +8,11 @@ import './index.css';
 // import { SimpleForm } from './02-useEffect/SimpleForm';
 // import {FormWithCustomHook} from './02-useEffect/FormWithCustomHook';
 // import {MultipleCustomHooks} from "./03-example/MultipleCustomHooks.jsx";
-import {FocusScreen} from "./04-useRef/FocusScreen.jsx";
+// import {FocusScreen} from "./04-useRef/FocusScreen.jsx";
+import {Layout} from "./05-useLayoutEffect/Layout.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     // <React.StrictMode>
-    <FocusScreen/>
+    <Layout/>
     // </React.StrictMode>
 );
