8d0ad1d224424acc476aec9774c3860e168ad0dd
Ricardo <ricardo.rv@outlook.com>
2022-11-05 10:18:33 +0100

    05-hook-app-vite MultipleCustomHooks UT

--- 05-hook-app-vite/tests/03-examples/MultipleCustomHooks.test.jsx
+++ 05-hook-app-vite/tests/03-examples/MultipleCustomHooks.test.jsx
@@ -1,8 +1,30 @@
-import {render, screen} from "@testing-library/react";
+import {fireEvent, render, screen} from "@testing-library/react";
 import {MultipleCustomHooks} from "../../src/03-example/MultipleCustomHooks";
+import {useCounter, useFetch} from "../../src/hooks";
+
+jest.mock('../../src/hooks/useFetch');
+jest.mock('../../src/hooks/useCounter');
 
 describe('MultipleCustomHooks UT', () => {
+    const mockIncrement = jest.fn();
+
+    useCounter.mockReturnValue({
+        increment: mockIncrement,
+        counter: 0
+    });
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    })
+
     test('should display the component by default', () => {
+
+        useFetch.mockReturnValue({
+            data: null,
+            hasError: null,
+            isLoading: true
+        });
+
         render(<MultipleCustomHooks/>);
         const nextButton = screen.getByRole('button', {name: 'Next quote'});
 
@@ -10,4 +32,41 @@ describe('MultipleCustomHooks UT', () => {
         expect(screen.getByText('breaking bad Quotes'));
         expect(nextButton.disabled).toBe(true);
     });
+
+    test('should display a Quote', () => {
+
+        useFetch.mockReturnValue({
+            data: [{
+                author: 'Ricardo',
+                quote: 'Hola que ases'
+            }],
+            hasError: false,
+            isLoading: false
+        });
+        render(<MultipleCustomHooks/>);
+        const nextButton = screen.getByRole('button', {name: 'Next quote'});
+        expect(screen.getByText('Ricardo'));
+        expect(screen.getByText('Hola que ases'));
+        expect(nextButton.disabled).toBe(false);
+    });
+
+    test('should call to increment function', () => {
+        useFetch.mockReturnValue({
+            data: [{
+                author: 'Ricardo',
+                quote: 'Hola que ases'
+            }],
+            hasError: false,
+            isLoading: false
+        });
+
+        render(<MultipleCustomHooks/>);
+        const nextButton = screen.getByRole('button', {name: 'Next quote'});
+        fireEvent.click(nextButton);
+
+        expect(mockIncrement).toHaveBeenCalled()
+
+    });
 });
+
+
